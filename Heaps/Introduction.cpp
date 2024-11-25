0adec6b83202050f6b1000e5ab48b6bb87e4d646
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2024-11-25 17:12:25 +0530

    Update Introduction.cpp

--- Heaps/Introduction.cpp
+++ Heaps/Introduction.cpp
@@ -1,1 +1,9 @@
 If there should be present of k and find smallest/largest then simply it is Heap
+
+Two types of Heap :
+  1) Min heap   k + largest then use Min heap
+  2) Max heap   k + smallest then use Max heap
+
+Kth Largest then - use Min heap  (smallest element will store on top and largest will store on bottom of min heap)
+
+If we use normal method then it will take around (nlogn)   ->   (nlogk)
