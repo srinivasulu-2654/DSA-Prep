82f838f2d36a49285367f26b83ef4073fabbcfc3
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2024-11-26 15:01:23 +0530

    Create 658. Find K Closest Elements.cpp

---
+++ Heaps/658. Find K Closest Elements.cpp
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/find-k-closest-elements/
+
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        
+        int n = arr.size();
+        priority_queue<pair<int,int>>maxi;
+        vector<int>ans;
+
+        for(int i=0;i<n;i++)
+        {
+            maxi.push({abs(arr[i] - x),arr[i]});
+
+            if(maxi.size()>k)
+            {
+                maxi.pop();
+            }
+        }
+
+        while(maxi.size() > 0)
+        {
+            auto it = maxi.top();
+            ans.push_back(it.second);
+            maxi.pop();
+        }
+
+       sort(ans.begin(),ans.end());
+       return ans;
+    }
+};
