ce6d28b95b7bbcfd1cf09e0c0765afaedb708cb7
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2024-11-28 15:51:24 +0530

    Create Introduction.cpp

---
+++ Dynamic Programming/0-1 Knapsack Problems/Introduction.cpp
@@ -0,0 +1,26 @@
+Knapsack Problem are 3 types :
+
+  1) Fractional knapsack problem -> comes in Greedy  -> in this approach we can take the item weights in fractional
+  2) 0/1 knapsack   -> item take or not take -> not allowed multiple occurences
+  3) unbounded knapsack problem -> item can take more than 1 times
+
+
+What is knapsack?
+  Simply it is a bag
+
+Problem will give these constraints:
+w[]  - weights of elements
+val[] - values of elements
+W   - Total capacity of the bag
+
+
+How to identify 0-1 knapsack problem:
+
+w[]  - weights of elements
+val[] - values of elements                - Choice  -> will take or not take
+W   - Total capacity of the bag
+
+max profit      -  Optimal 
+
+
+DP - Recursion + Storage
