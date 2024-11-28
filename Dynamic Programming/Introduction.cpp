22119c7087b311d6513e168a9f78b4d448c15353
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2024-11-28 15:39:27 +0530

    Create Introduction.cpp

---
+++ Dynamic Programming/Introduction.cpp
@@ -0,0 +1,16 @@
+Dp- Enchnaced Recursion 
+
+DP parent - Recursion
+
+Identification : 
+  1.choice I mean include or not include
+  2. Optimal - (max/min/largest/smallest) optimal poochega
+   If recursive will be 2 call then there occurs some overalapping problems(overlapping recursion calls)
+   If there is only single call then there won't be DP(/
+                                                        /
+                                                        /
+                                                        /) like this is the single recursive calls (same as skew tree)
+
+
+Important points to note :
+  ->First write in Recursive -> memoize -> top-down
