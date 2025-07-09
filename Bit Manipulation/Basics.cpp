abd60297019d42d97c3751e803e4fef324a0337b
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2025-07-09 06:24:02 +0530

    Create Basics.cpp

---
+++ Bit Manipulation/Basics.cpp
@@ -0,0 +1,15 @@
+/*
+
+ -> so here if we want to write (13) base 10 in binary form how can we like  will do LCM 
+ -> like whatever the remainder that means 13 it will divied with 2 * 6 then remainder will come as 1 so this is binary form and also should write from bottom to top
+ -> (1 1 0 1) base 2 convert into decimal na should do from last to first
+
+ Q) Explore the question of convert decimal to binary? TC : O(log2n) because it is dividing half half eveytime, SC : Same becuase we are storing in string
+ Q) Explore the question of convert binary to decimal?
+
+
+ -> SO here 1 important thing like computer only stores 0's and 1's
+ -> so if we want to print print(x) that means the binary converts into decimal then it shows in display as decimal form
+ -> int x = 13 so  1 1 0 1 this is 4 bits but actual memory is 32 so remaining all will store 0's
+
+*/
