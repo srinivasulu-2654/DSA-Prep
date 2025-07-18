b707636058bcad4c9351157d905efc1e4f15d101
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2025-07-18 07:10:09 +0530

    Update Count set bits.cpp

--- Bit Manipulation/Count set bits.cpp
+++ Bit Manipulation/Count set bits.cpp
@@ -1,5 +1,5 @@
 /*
-  Bruete Force:
+  1. Bruete Force:
 
   int countSetBits(int n) {
         // code here
@@ -20,10 +20,19 @@
         return cnt;
     }
 
-  -> Actually tha above code is to count set bits from 1 to n
+  2. Method-> Actually tha above code is to count set bits from 1 to n
  
   1. Here actually for odd numbers na the last bit bit will be for sure 1 only 
   2. if we check for 13 = 8 + 4 + 1
   3. so here to remainder 1 we can do for n & 1 because for even na 14 %2 = 0 no use of incrementing count and if it is 13 na 1101 & 1 it will give 1
   4. for n = n/2 dividing we can do right shift operator n >> 1 => n/2
+
+  3. Brian Kernighan's Algorithm  
+
+  while(n != 0){
+  n = n&(n-1)
+  cnt++;
+  }
+  return cnt;
+  }
 */
