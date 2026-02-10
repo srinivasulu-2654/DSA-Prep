a34c2d19faba340a0343f6e9fe78f46fdbda2223
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-02-10 22:03:34 +0530

    Create 019-Count Inversions.cpp

---
+++ Arrays/Coding Part/019-Count Inversions.cpp
@@ -0,0 +1,55 @@
+class Solution {
+  public:
+  
+  int cnt = 0;
+  
+  void merge(vector<int>&arr,int low,int mid,int high){
+      
+      vector<int>temp;
+      int left = low;
+      int right = mid+1;
+      
+      while(left<=mid && right<=high){
+          if(arr[left]<=arr[right])
+          {
+              temp.push_back(arr[left]);
+              left++;
+          }
+          else{
+              temp.push_back(arr[right]);
+              cnt += mid-left+1;
+              right++;
+          }
+      }
+      
+      while(left<=mid){
+          temp.push_back(arr[left]);
+          left++;
+      }
+      
+      while(right<=high){
+          temp.push_back(arr[right]);
+          right++;
+      }
+      
+      for(int i=low;i<=high;i++){
+          arr[i] = temp[i-low];
+      }
+      
+  }
+  
+  void mergeSort(vector<int>&arr,int low,int high){
+      if(low>=high) return;
+      int mid = (low+high)/2;
+      mergeSort(arr,low,mid);
+      mergeSort(arr,mid+1,high);
+      merge(arr,low,mid,high);
+  }
+  
+    int inversionCount(vector<int> &arr) {
+        // Code Here
+        mergeSort(arr,0,arr.size()-1);
+        return cnt;
+        
+    }
+};
