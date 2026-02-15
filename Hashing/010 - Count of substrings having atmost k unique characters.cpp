b16623f6232aaa51df29245781c8934ef3caefd9
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-02-15 15:27:19 +0530

    Update 010 - Count of substrings having atmost k unique characters.cpp

--- Hashing/010 - Count of substrings having atmost k unique characters.cpp
+++ Hashing/010 - Count of substrings having atmost k unique characters.cpp
@@ -48,40 +48,25 @@ Optimal approach:
 #include<bits/stdc++.h>
 using namespace std;
 
-int substrings_having_atmost_k_unique_characters(string s,int k)
+int substrings_having_atmost_k_unique_characters(string &s,int k)
 {
     int n = s.size();
     int cnt = 0;
     unordered_map<char,int>mp;
     int i=0,j=0;
-    int distinct = 0;
     while(j<n)
     {
+        mp[s[j]]++;
         
-        if(mp.find(s[j])!=mp.end()) 
-        {
-            mp[s[j]]++;
-        }
-        else{
-            mp[s[j]] = 1;
-            distinct++;
-        }
-        
-        while(i<j && distinct > k)
+        while(mp.size()>k)
         {
             mp[s[i]]--;
-            if(mp[s[i]] == 0)
-            {
-                distinct--;
-                mp.erase(s[i]);
-                
-            }
+            if(mp[s[i]]==0) mp.erase(s[i]);
             i++;
         }
         
         cnt += j-i+1;
         j++;
-         
     }
     
     return cnt;
