671eee99f5dcc19800dec0b0b0640add1c59b240
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-03-12 23:06:40 +0530

    Create Concept.cpp

---
+++ Disjoint Set Union/Concept.cpp
@@ -0,0 +1,19 @@
+/*
+  Disjoint Set Union(DSU)
+
+  Disjoint sets:
+  Ex: -> there is set of Apples in set 1
+      -> set of lcihies in set 2
+      -> set of mangaoes in set 3
+      so there s1^s2^s3 == NULL (called as disjoint sets) ^-> means intersection
+
+      DSU Helps:
+      ->to combine 2 sets:
+      ->to tell if 2 members for example(b,g) belongs to same set or not -> callled "union find"
+
+    element a b c d e f g h
+    parent  a b c d e f g h
+
+    -> as we know 2 operations
+
+*/
