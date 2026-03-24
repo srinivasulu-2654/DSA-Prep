dba73208d2afbaded704b1e447bdb3e9010419da
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-03-24 21:05:35 +0530

    Update 002 - Search a 2D Matrix.cpp

--- Binary_search/BS on 2D Arrays/002 - Search a 2D Matrix.cpp
+++ Binary_search/BS on 2D Arrays/002 - Search a 2D Matrix.cpp
@@ -1,3 +1,17 @@
+/*
+    -> other than this solution just faltten the 2D matrix in single array imagine
+    -> l = 0, h = m*n-1 (just imagination) -> think it in a brain
+    -> also we have to get to know row and col based on mid
+    -> we have to convert 1D coordiate into 2D coordinate that's it our job is done
+    -> row = mid/m
+    -> col = mid%m -> here m means length of coloumn 
+    -> so here 1st col i mean 0th col having multiples of 4 if n = 3, m = 4
+    -> thus mid / m will get row
+    -> col => mid % m
+*/
+
+// Bruete Force
+
 class Solution {
 public:
 
@@ -25,6 +39,32 @@ public:
             bool isFound = isPresent(matrix[i],target);
             if(isFound == true) return true;
         }
+        return false;
+    }
+};
+
+// Optimal Approach :
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int l = 0,h=(m*n)-1;
+
+
+        while(l<=h)
+        {
+             int mid = (l+h)/2;
+             int row = mid/m;
+             int col = mid%m;
+
+             if(matrix[row][col] == target) return true;
+             else if(matrix[row][col] >= target) h = mid - 1;
+             else l = mid + 1;
+        }
+
         return false;
     }
 };
