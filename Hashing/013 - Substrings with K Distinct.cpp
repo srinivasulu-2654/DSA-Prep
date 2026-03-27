8a0a2aac45f851ea3370e7e6b9b5a1e871fd732b
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-03-27 23:48:44 +0530

    Update 013 - Substrings with K Distinct.cpp

--- Hashing/013 - Substrings with K Distinct.cpp
+++ Hashing/013 - Substrings with K Distinct.cpp
@@ -1,3 +1,44 @@
 https://www.geeksforgeeks.org/problems/count-number-of-substrings4528/1
 
 We have to   do this question also 
+
+/*
+  -> Here it is coming TLE if we are using that hashmap things and all instead we can use the freq array
+  -> we can able to track the distinct keyword and then will track
+*/
+
+  class Solution {
+  public:
+  
+    int substrings_having_atmost_k_unique_characters(string &s,int k)
+{
+    int n = s.size();
+    int cnt = 0;
+   vector<char>freq(26,0);
+   int distinct = 0;
+    int i=0,j=0;
+    while(j<n)
+    {
+        if(freq[s[j] - 'a'] == 0) distinct++;
+        freq[s[j] - 'a']++;
+        
+        while(distinct>k)
+        {
+            freq[s[i]-'a']--;
+            if(freq[s[i] - 'a'] == 0) distinct--;
+            i++;
+        }
+        
+        cnt += j-i+1;
+        j++;
+    }
+    
+    return cnt;
+}
+  
+    int countSubstr(string& s, int k) {
+        // code here
+        int res = substrings_having_atmost_k_unique_characters(s,k) - substrings_having_atmost_k_unique_characters(s,k-1);
+        return res;
+    }
+};
