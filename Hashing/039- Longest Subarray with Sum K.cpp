b00a99064608b8972e929cd41a274d69eed89286
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-03-28 00:14:54 +0530

    Update 039- Longest Subarray with Sum K.cpp

--- Hashing/039- Longest Subarray with Sum K.cpp
+++ Hashing/039- Longest Subarray with Sum K.cpp
@@ -1,5 +1,39 @@
 https://www.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1
 
+Code 1 :
+
+This code only works for all positive integers example: vector<int> nums = {10, 5, 2, 7, 1, 9};
+but for negative integers it wont work we should do only code 2 : [10, 5, 2, 7, 1, -10], k = 15
+
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        // code here
+        
+       int n = arr.size();
+       int maxi = 0;
+       int i=0,j=0;
+       int currSum = 0;
+       while(j<n)
+       {
+           currSum += arr[j];
+           
+           while(currSum>k)
+           {
+               currSum -= arr[i];
+               i++;
+           }
+           
+           if(currSum == k) maxi = max(maxi,j-i+1);
+           j++;
+       }
+       
+       return maxi;
+    }
+};
+
+Code 2 : 
+
 class Solution {
   public:
     int longestSubarray(vector<int>& arr, int k) {
