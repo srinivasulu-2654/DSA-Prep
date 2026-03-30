762683f754418c7a4db7a7910ec423a07ad63145
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-03-30 21:45:35 +0530

    Create 004 - Implement Queue using stack.cpp

---
+++ Stack And Queues/004 - Implement Queue using stack.cpp
@@ -0,0 +1,13 @@
+/* 
+    -> here we have to use 2 stacks
+    -> s1 and  s2
+    -> for push:
+        Steps:
+        -> s1->s2;
+        -> x -> s1;
+        -> s2 -> s1
+
+    -> so here queue follows FIFO and stack follows LIFO right
+    -> so that's why we are doing this thing
+
+*/
