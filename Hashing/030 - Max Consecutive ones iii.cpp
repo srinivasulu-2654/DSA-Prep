2a29e52297e9c4090e82cf2fed338365f05f75b8
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-03-30 13:01:26 +0530

    Update 030 - Max Consecutive ones iii.cpp

--- Hashing/030 - Max Consecutive ones iii.cpp
+++ Hashing/030 - Max Consecutive ones iii.cpp
@@ -1,1 +1,26 @@
 // Try this problem also for sure
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxi = 0;
+        int i=0,j=0;
+        int zerocnt = 0;
+        while(j<n)
+        {
+            if(nums[j]==0) zerocnt++;
+
+            while(zerocnt>k)
+            {
+                if(nums[i]==0) zerocnt--;
+                i++;
+            }
+
+            maxi = max(maxi,j-i+1);
+            j++;
+        }
+
+        return maxi;
+    }
+};
