2d5eb7144c07fa84088c02f2e2a7f68f6748bc95
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-03-30 14:59:35 +0530

    Update 033 - Fruit into Baskets.cpp

--- Hashing/033 - Fruit into Baskets.cpp
+++ Hashing/033 - Fruit into Baskets.cpp
@@ -1,6 +1,11 @@
 // https://leetcode.com/problems/fruit-into-baskets/
 // so this problem just gave theory more but compare this problem is same as:
 // longest continuous subarray that contains at most 2 distinct numbers.
+// so here the thing is we have to collect of the same type in 2 baskets:
+// for example fruits = [0, 1, 2, 2] 0 type , 1 type 2 type but we have only 2 baskets
+// if we start from 0 we can collect only 0 and 1 not 2 type
+// if we start from 1 index we can collect all 1's and 2's
+// so this the longest contigous subarry with contains atmost 2
 
 class Solution {
 public:
