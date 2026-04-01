267bcc99c2850c84cfed5d6d3c42b7c0fc773793
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-04-01 14:53:40 +0530

    Update and rename Nearly sorted.cpp to 003 - Nearly sorted.cpp

--- Heaps/003 - Nearly sorted.cpp
+++ Heaps/003 - Nearly sorted.cpp
@@ -1,6 +1,10 @@
 https://www.geeksforgeeks.org/problems/nearly-sorted-1587115620/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
 
-
+/*
+    -> here we want to place the values in original place
+    -> i mean k aslo given but the k will be in the place of that value to till k
+    -> so take mini and put top into the arr
+*/
 
     void nearlySorted(vector<int>& arr, int k) {
         // code
