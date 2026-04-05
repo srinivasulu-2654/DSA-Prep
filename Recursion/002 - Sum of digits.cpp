c0104a178c55d50058a1076ba5f777a6e6b6994f
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-04-05 18:17:29 +0530

    Create 002 - Sum of digits.cpp

---
+++ Recursion/002 - Sum of digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int sumOfDigits(int n) {
+        // code here
+        
+        if(n==0) return 0;
+        
+        
+        
+        return n%10 + sumOfDigits(n/10);
+    }
+};
