3d079f7c82bc61e8c59e7a5156c1b65f06e052b9
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-04-05 12:11:16 +0530

    Create 001 - Detonate the Maximum Bombs.cpp

---
+++ Graphs/Interview Questions/001 - Detonate the Maximum Bombs.cpp
@@ -0,0 +1,75 @@
+/*
+  -> This question was asked in my amazon interview sde 1 on 3 April 2026
+  -> I was literally blank by seeing this question
+  -> main approach is if we choose 1 bomb to detonate then how many maximum bombs will detonate we have to return that
+  -> so if there are to bombs in different radius for that we have to calculate the distance d
+  -> if r >= d then only if we detonate 1 bomb it will effect other too also 
+  -> Why Graph problem ? : ************ IMPORTANT ***************
+  -> let's say we have bombs b1,b2,b3,b4 like that
+  -> then we used to make adjacency list in graph right?
+  -> hence if B1 -> {B2,B3}, B2 -> {B4,B5}
+  -> another question will it be directed or undirected (that's why directed graph only -> check below 2 points)
+  -> if b1 in the radius b2 also there then both will blast
+  -> if b1 is not in the radius of b2 then b2 radius < b1 radius => then only b2 will blast
+  -> we are visiting  the nodes
+  -> no. of nodes you are able to visit from a given node = detonate count
+  -> in question we have blast only 1 bomb then from that bomb how many maximum will blast
+  -> so we have to call each bomb to the dfs or bfs then caluclate among the maximum
+*/
+
+DFS :
+
+class Solution {
+public:
+    typedef long long ll;
+    void dfs(int u,unordered_set<int>&vis,unordered_map<int,vector<int>>&adj)
+    {
+        vis.insert(u);
+        for(int v:adj[u]){
+            if(vis.find(v)==vis.end())
+            {
+                dfs(v,vis,adj);
+            }
+        }
+    }
+
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        
+        int n = bombs.size();
+        unordered_map<int,vector<int>>adj;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j) continue;
+
+                ll x1 = bombs[i][0];
+                ll y1 = bombs[i][1];
+                ll r1 = bombs[i][2];
+
+                ll x2 = bombs[j][0];
+                ll y2 = bombs[j][1];
+                ll r2 = bombs[j][2];
+
+                ll distance = (x2-x1) * (x2-x1) + (y2-y1) * (y2-y1);
+                if(r1*r1 >= distance)
+                {
+                    adj[i].push_back(j);
+                }
+
+            }
+        }
+
+        int res = 0;
+        unordered_set<int>vis;
+        for(int i=0;i<n;i++)
+        {
+            dfs(i,vis,adj);
+            int cnt = vis.size();
+            res = max(cnt,res);
+            vis.clear(); // should clear for each and eve
+        }
+
+        return res;
+    }
+};
