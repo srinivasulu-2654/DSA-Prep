37084c1d55a38346b824f8058bba7d2a2c6f308a
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-04-21 22:40:11 +0530

    Create 000 - Concept.cpp

---
+++ Graphs/PatternWise/000 - Concept.cpp
@@ -0,0 +1,6 @@
+/*
+  -> Graph is data structure container stores info -> nodes/vertices and edges 
+  -> Best example of graph is BinaryTree
+  -> go through the articles in graph such as concepts, uses of graph 
+  -> 
+*/
