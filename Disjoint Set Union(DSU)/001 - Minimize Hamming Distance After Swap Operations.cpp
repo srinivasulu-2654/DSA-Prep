e248cd33cb159aa9c2fbf10c6a0a9d409c0204fc
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2026-04-21 21:07:33 +0530

    Create 001 - Minimize Hamming Distance After Swap Operations.cpp

---
+++ Disjoint Set Union(DSU)/001 - Minimize Hamming Distance After Swap Operations.cpp
@@ -0,0 +1,15 @@
+/*
+  -> Here the idea is we have to swap the source array and should equal to target if not we can return the difference count
+  -> source = {1,2,3,4}, target = {2,2,4,5} allowedswaps = {(0,1),(2,3)}
+  -> make the components as parents and look into that 
+  -> here store the map of the frequency
+  -> map<int,int>  => (component,{num->freq})
+
+  Story Points :
+  -> DSU -> find/union (rank/pathcom)
+  -> unordered_map<int,unordered_map<int,int>>mp
+  -> traverse the array
+      int parent = find(i);
+      groupFreq[parent][source[i]]++;
+  -> hamming distance = 0;
+*/
